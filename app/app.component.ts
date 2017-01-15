4fe7162c74674dfa2621fd1636b9c980c0414804
Edison <edison@skiploop.com>
2017-01-15 15:18:29 +0530

    Display Heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Hero} from './hero'
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    My Favorite Hero is : <b>{{myHero.name}}</b>
+    <p *ngIf="heroes.length>3">There are many Heroes.</p>
+    <ul>
+    <li *ngFor="let hero of heroes">{{hero.name}}</li>
+    </ul>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+    title:string = 'Tour of Heroes';
+    heroes:Hero[] = [
+        new Hero(1, 'Windstorm'),
+        new Hero(13, 'Bombasto'),
+        new Hero(15, 'Magneta'),
+        new Hero(20, 'Tornado')
+    ];
+    myHero = this.heroes[0];
+}
